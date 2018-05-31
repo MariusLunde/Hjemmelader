9290ebcf028ffaa617ed312a1b8659fdb7a13863
Marius Lunde <mariusalunde@gmail.com>
2018-05-31 18:30:43 +0200

    fjernet ekstra tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import {MinBilPage} from "../min-bil/min-bil";
-import {MittSikringsskapPage} from "../mitt-sikringsskap/mitt-sikringsskap";
 import {NEASProdukterPage} from "../neasprodukter/neasprodukter";
 import {KontaktOssPage} from "../kontakt-oss/kontakt-oss";
 
@@ -10,7 +9,6 @@ import {KontaktOssPage} from "../kontakt-oss/kontakt-oss";
 })
 export class TabsPage {
     tab1Root: any = MinBilPage;
-    tab2Root: any = MittSikringsskapPage;
     tab3Root: any = NEASProdukterPage;
     tab4Root: any = KontaktOssPage;
 
