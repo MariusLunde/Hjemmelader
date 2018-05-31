b57de5d1cee85dcc17f6ba89f90c5485ae4e829c
Marius Lunde <mariusalunde@gmail.com>
2018-06-01 01:08:40 +0200

    Fjernet resterende console dir

--- src/pages/ceh/ceh.ts
+++ src/pages/ceh/ceh.ts
@@ -17,9 +17,4 @@ export class CehPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CehPage');
-  }
-
 }
--- src/pages/wbcd/wbcd.ts
+++ src/pages/wbcd/wbcd.ts
@@ -17,9 +17,4 @@ export class WbcdPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad WbcdPage');
-  }
-
 }
