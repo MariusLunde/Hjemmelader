524d47de9aaf929054353abcd64f2bbf1966b4e7
Marius Lunde <mariusalunde@gmail.com>
2018-06-04 10:47:06 +0200

    la til ladetid

--- src/pages/resultat/resultat.ts
+++ src/pages/resultat/resultat.ts
@@ -20,6 +20,9 @@ export class ResultatPage {
     public ladeAmpere: string;
     public ladeKurs: string;
     public effect: number;
+    public ladeKap: number;
+    public midLade: number;
+    public ladeTid: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
       this.bilValg = this.navParams.get('bilvalg');
@@ -27,11 +30,54 @@ export class ResultatPage {
       this.ladeAmpere = this.navParams.get('laderAmpere');
       this.ladeKurs = this.navParams.get('ladeKurs');
 
+      if(this.bilValg == 'BMW i3') {
+          this.ladeKap = 18800;
+      }else if(this.bilValg == 'Ford Focus Electric (2013-2018)') {
+          this.ladeKap = 29500;
+      }else if(this.bilValg == 'Hyundai IONIQ Electric') {
+          this.ladeKap = 28000;
+      }else if(this.bilValg == 'Kia Soul Electric') {
+          this.ladeKap = 30000;
+      }else if(this.bilValg == 'Mercedes-Benz B250e (2015-2017)') {
+          this.ladeKap = 28000;
+      }else if(this.bilValg == 'Mitsubishi i-MiEV') {
+          this.ladeKap = 16000;
+      }else if(this.bilValg == 'Nissan e-NV200 Evalia') {
+          this.ladeKap = 24000;
+      }else if(this.bilValg == 'Nissan LEAF (24 og 30 kWh, 2011-2017)') {
+          this.ladeKap = 21600;
+      }else if(this.bilValg == 'Nissan LEAF (40 kWh)') {
+          this.ladeKap = 36000;
+      }else if(this.bilValg == 'Opel Ampera-e') {
+          this.ladeKap = 17600;
+      }else if(this.bilValg == 'Peugeot iOn / Citroën C-Zero') {
+          this.ladeKap = 14400;
+      }else if(this.bilValg == 'Renault Zoe R90 Z.E. 40') {
+          this.ladeKap = 41000;
+      }else if(this.bilValg == 'Smart ForFour Electric Drive') {
+          this.ladeKap = 17600;
+      }else if(this.bilValg == 'Tesla Model S 75 (D)') {
+          this.ladeKap = 67500;
+      }else if(this.bilValg == 'Tesla Model S 90D og P100D') {
+          this.ladeKap = 82000;
+      }else if(this.bilValg == 'Tesla Model X 90D og P100D') {
+          this.ladeKap = 100000;
+      }else if(this.bilValg == 'Volkswagen e-Golf') {
+          this.ladeKap = 31500;
+      }else if(this.bilValg == 'Volkswagen e-up!') {
+          this.ladeKap = 16800;
+      }
+
+
       if(this.ladeKurs == '400V3'){
         this.effect = 690 * parseInt(this.ladeAmpere);
       }else {
         this.effect = 230 * parseInt(this.ladeAmpere);
       }
+
+      this.midLade = this.ladeKap / this.effect;
+
+      this.ladeTid = Math.round(this.midLade);
   }
 
   ionViewDidLoad() {
